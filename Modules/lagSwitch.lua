36a7ed8f8f18eef6bcda193a3aa9a06462f4b8b4
WillDrac <willdrac@yahoo.com>
2022-08-25 10:18:05 -0400

    lag switch :skull:

--- Modules/lagSwitch.lua
+++ Modules/lagSwitch.lua
@@ -4,6 +4,7 @@ name = "Lag Switch"
 description = "Freezes your game for a bit."
 
 event.listen("KeyboardInput", function(key, down)
+    -- M
     if down and key == 77 then
         for i = 1, 200, 1 do
             for j = 1, 200, 1 do
