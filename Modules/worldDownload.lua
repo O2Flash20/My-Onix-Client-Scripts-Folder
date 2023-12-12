5950abc92b8a91f746ffec4da04dcb48a1a9f4a0
WillDrac <willdrac@yahoo.com>
2023-12-12 09:12:52 -0500

    im gonna want to do this later

---
+++ Modules/worldDownload.lua
@@ -0,0 +1,10 @@
+name = "World Downloader"
+description = "Downloads a section of the world you're in, it can be loaded into a creative world later."
+
+importLib("vectors")
+importLib("logger")
+
+
+function update()
+
+end
