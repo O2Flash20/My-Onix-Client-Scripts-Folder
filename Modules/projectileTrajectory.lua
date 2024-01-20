3356c529d08b07ab562c1061c4caaac221bb727d
WillDrac <willdrac@yahoo.com>
2024-01-19 21:45:03 -0500

    added settings, stop when hit, arrows, auto on/off

--- Modules/projectileTrajectory.lua
+++ Modules/projectileTrajectory.lua
@@ -1,20 +1,14 @@
+-- Made By O2Flash20 🙂
+
 name = "Projectile Trajectory"
 description = "Shows you where your projectiles will hit"
 
-importLib("renderThreeD")
-importLib("logger")
+col = client.settings.addNamelessColor("Trajectory Color", { 255, 0, 0, 255 })
+col2 = client.settings.addNamelessColor("Target Color", { 255, 255, 255, 255 })
 
--- initial velocity: magnitude of 30m/s
--- acceleration due to gravity: -12m/(s^2)
-
--- SNOWBALL
-INITIALVEL = 30
-GRAVACC = 11.5
-TERMVEL = 54.5
-
--- ENDER PEARL (PERFECT)
--- INITIALVEL = 45
--- GRAVACC = 22
+info =
+"Notes: \n- Arrows have a bit of randomness that cannot be predicted.\n- I can only assure that this works on vanilla Minecraft worlds, servers may have different physics."
+client.settings.addInfo(info)
 
 function calculateDeltaX(initialVelocity, angle, time)
     return initialVelocity * math.cos(angle) * time
@@ -36,34 +30,118 @@ function calculateDeltaYDrag(initalVelocity, angle, terminalVelocity, accelerati
         terminalVelocity * time
 end
 
-function render3d()
-    local px, py, pz = player.pposition()
-    local pyaw, ppitch = player.rotation()
-    local points = {}
-    -- for i = 0, 10, 0.1 do
-    --     local deltaX = calculateDeltaX(INITIALVEL, -math.rad(ppitch), i) --BUT, this is split between x and z, depending on pyaw
-    --     table.insert(points, {
-    --         deltaX * math.sin(math.rad(-pyaw)),
-    --         calculateDeltaY(INITIALVEL, -math.rad(ppitch), GRAVACC, i),
-    --         deltaX * math.cos(math.rad(-pyaw))
-    --     })
-    -- end
+timeIncrement = 0.01
+timeMax = 20
 
-    for i = 0, 10, 0.1 do
-        local deltaX = calculateDeltaXDrag(INITIALVEL, -math.rad(ppitch), TERMVEL, GRAVACC, i) --BUT, this is split between x and z, depending on pyaw
-        table.insert(points, {
-            deltaX * math.sin(math.rad(-pyaw)),
-            calculateDeltaYDrag(INITIALVEL, -math.rad(ppitch), TERMVEL, GRAVACC, i),
-            deltaX * math.cos(math.rad(-pyaw))
-        })
+function render3d()
+    shouldDrawTarget = false
+
+    local px, py, pz = player.forwardPosition(0.001)
+    local pyaw, ppitch = player.rotation()
+
+    local selectedItem = player.inventory().selectedItem()
+    if selectedItem == nil then return end
+
+    local points = {}
+    if selectedItem.id == 377 or selectedItem.id == 393 then --smowball and eggs
+        INITIALVEL = 30
+        GRAVACC = 11.5
+        TERMVEL = 54.5
+        local lastX, lastY, lastZ = nil, nil, nil
+        for i = 0, timeMax, timeIncrement do
+            local deltaX = calculateDeltaXDrag(INITIALVEL, -math.rad(ppitch), TERMVEL, GRAVACC, i) --BUT, this is split between x and z, depending on pyaw
+            local x = deltaX * math.sin(math.rad(-pyaw))
+            local y = calculateDeltaYDrag(INITIALVEL, -math.rad(ppitch), TERMVEL, GRAVACC, i)
+            local z = deltaX * math.cos(math.rad(-pyaw))
+
+            if lastX and lastY and lastZ then
+                local raycast = dimension.raycast(lastX + px, lastY + py, lastZ + pz, x + px, y + py, z + pz)
+                if raycast.isBlock or raycast.isEntity then
+                    table.insert(points, { raycast.px - px, raycast.py - py, raycast.pz - pz })
+                    goto drawLine
+                else
+                    table.insert(points, { x, y, z })
+                end
+            else
+                table.insert(points, { x, y, z })
+            end
+
+            lastX, lastY, lastZ = x, y, z
+        end
+    elseif selectedItem.id == 425 then --ender pearls
+        INITIALVEL = 45
+        GRAVACC = 22
+        local lastX, lastY, lastZ = nil, nil, nil
+        for i = 0, timeMax, timeIncrement do
+            local deltaX = calculateDeltaX(INITIALVEL, -math.rad(ppitch), i) --BUT, this is split between x and z, depending on pyaw
+            local x = deltaX * math.sin(math.rad(-pyaw))
+            local y = calculateDeltaY(INITIALVEL, -math.rad(ppitch), GRAVACC, i)
+            local z = deltaX * math.cos(math.rad(-pyaw))
+
+            if lastX and lastY and lastZ then
+                local raycast = dimension.raycast(lastX + px, lastY + py, lastZ + pz, x + px, y + py, z + pz)
+                if raycast.isBlock or raycast.isEntity then
+                    table.insert(points, { raycast.px - px, raycast.py - py, raycast.pz - pz })
+                    goto drawLine
+                else
+                    table.insert(points, { x, y, z })
+                end
+            else
+                table.insert(points, { x, y, z })
+            end
+
+            lastX, lastY, lastZ = x, y, z
+        end
+    elseif selectedItem.id == 303 or selectedItem.id == 583 then --bow and crossbow
+        INITIALVEL = 83.6
+        GRAVACC = 11.5
+        TERMVEL = 54.5
+        local lastX, lastY, lastZ = nil, nil, nil
+        for i = 0, timeMax, timeIncrement do
+            local deltaX = calculateDeltaXDrag(INITIALVEL, -math.rad(ppitch), TERMVEL, GRAVACC, i) --BUT, this is split between x and z, depending on pyaw
+            local x = deltaX * math.sin(math.rad(-pyaw))
+            local y = calculateDeltaYDrag(INITIALVEL, -math.rad(ppitch), TERMVEL, GRAVACC, i)
+            local z = deltaX * math.cos(math.rad(-pyaw))
+            if lastX and lastY and lastZ then
+                local raycast = dimension.raycast(lastX + px, lastY + py, lastZ + pz, x + px, y + py, z + pz)
+                if raycast.isBlock or raycast.isEntity then
+                    table.insert(points, { raycast.px - px, raycast.py - py, raycast.pz - pz })
+                    goto drawLine
+                else
+                    table.insert(points, { x, y, z })
+                end
+            else
+                table.insert(points, { x, y, z })
+            end
+
+            lastX, lastY, lastZ = x, y, z
+        end
     end
 
-
-    gfx.color(255, 0, 0)
-    for i = 1, #points - 1, 1 do
-        gfx.line(
-            points[i][1] + px, points[i][2] + py, points[i][3] + pz,
-            points[i + 1][1] + px, points[i + 1][2] + py, points[i + 1][3] + pz
+    ::drawLine::
+    if selectedItem.id == 425 or selectedItem.id == 393 or selectedItem.id == 377 or selectedItem.id == 303 or selectedItem.id == 583 then
+        gfx.color(col.value.r, col.value.g, col.value.b, col.value.a)
+        for i = 1, #points - 1, 1 do
+            gfx.line(
+                points[i][1] + px, points[i][2] + py, points[i][3] + pz,
+                points[i + 1][1] + px, points[i + 1][2] + py, points[i + 1][3] + pz
+            )
+        end
+        shouldDrawTarget = true
+        targetX, targetY = gfx.worldToScreen(
+            points[#points][1] + px,
+            points[#points][2] + py,
+            points[#points][3] + pz
         )
+    end
+end
+
+shouldDrawTarget = false
+targetX, targetY = nil, nil
+
+function render()
+    if shouldDrawTarget and targetX and targetY then
+        gfx.color(col2.value.r, col2.value.g, col2.value.b, col2.value.a)
+        gfx.text(targetX - 2.4, targetY - 4, "x")
     end
 end
